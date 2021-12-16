834d3780294a0231aca80fcdfd56916a45b6ce1a
Andrei Constantinescu <andreiec112@gmail.com>
2021-12-16 16:07:42 +0200

    Added right panel items and made structure for custom title bar

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow
 
+
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({ width: 1280, height: 898, autoHideMenuBar: true, resizable: false })
+    mainWindow = new BrowserWindow({ width: 1280, height: 869, autoHideMenuBar: true, resizable: false, titleBarStyle: "hidden" })
     mainWindow.loadFile('main.html')
 })
---
+++ main.js
@@ -0,0 +1,5 @@
+const titlebar = require('custom-electron-titlebar')
+
+new titlebar.Titlebar({
+    backgroundColor: titlebar.Color.fromHex('#2b2b38')
+})
