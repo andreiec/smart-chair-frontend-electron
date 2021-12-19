06cb150e3f53c7dc1b7c0748361732b5777a25b2
Andrei Constantinescu <andreiec112@gmail.com>
2021-12-19 23:18:44 +0200

    Added integration of three js for 3d rendering. Setup canvas for rendering. Rendered basic 3d cube

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ table_height_minus.addEventListener("click", () => {
     }
 })
 
-// Table height buttons
+// Chair Heating buttons
 chair_heating_plus.addEventListener("click", () => {
     if (chair_heating_value_content < 25){
         chair_heating_value_content++
@@ -76,3 +76,47 @@ chair_heating_minus.addEventListener("click", () => {
         chair_heating_value.textContent = chair_heating_value_content + "°C"
     }
 })
+
+
+//
+// THREE.JS RENDERING
+//
+
+
+// Canvas size
+canvas_width = 620
+canvas_height = 445
+
+// Define scene camera and renderer
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, canvas_width / canvas_height, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer( {antialias: true, alpha: true } )
+const canvas = document.getElementById("renderer-canvas")
+
+renderer.setSize(canvas_width, canvas_height)
+
+// Transparent background
+renderer.setClearColor(0x000000, 0)
+
+// Append renderer
+canvas.appendChild(renderer.domElement)
+
+// Create cube
+const texture = new THREE.TextureLoader().load( 'crate.gif' );
+const geometry = new THREE.BoxGeometry(2, 2, 2)
+const material = new THREE.MeshBasicMaterial( {map: texture } )
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 5
+
+// Animate cube
+function animate() {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+
+    renderer.render(scene, camera)
+}
+
+animate()
