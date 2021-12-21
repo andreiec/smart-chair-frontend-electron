0f87607e76a0cf17eb0fc59f650fe033ab768f1c
Andrei Constantinescu <andreiec112@gmail.com>
2021-12-21 16:24:57 +0200

    Finished rendering of 3D Chair. Added orbit controls with custom settings, fixed ligthing. Added automatic rotation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-const electron = require('electron')
+const electron = require('electron');
+import { Scene, PerspectiveCamera, WebGLRenderer, AxesHelper, HemisphereLight, ReinhardToneMapping, SpotLight, Vector3 } from './three.module.js';
+import { GLTFLoader } from './GLTFLoader.js';
+import { OrbitControls } from './OrbitControls.js';
 
 const { ipcRenderer } = electron
 
@@ -84,39 +87,65 @@ chair_heating_minus.addEventListener("click", () => {
 
 
 // Canvas size
-canvas_width = 620
-canvas_height = 445
+const canvas_width = 620
+const canvas_height = 445
 
 // Define scene camera and renderer
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, canvas_width / canvas_height, 0.1, 1000)
-const renderer = new THREE.WebGLRenderer( {antialias: true, alpha: true } )
+const scene = new Scene()
+const camera = new PerspectiveCamera(75, canvas_width / canvas_height, 0.1, 1000)
+const renderer = new WebGLRenderer( {antialias: true, alpha: true } )
 const canvas = document.getElementById("renderer-canvas")
 
-renderer.setSize(canvas_width, canvas_height)
+// Adding lighting
+const hemiLight = new HemisphereLight(0xffeeb1, 0x080820, 7.2)
+scene.add(hemiLight)
 
-// Transparent background
+
+// Set renderer size and background
+renderer.setSize(canvas_width, canvas_height)
 renderer.setClearColor(0x000000, 0)
+renderer.toneMapping = ReinhardToneMapping
+renderer.toneMappingExposure = 1
 
 // Append renderer
 canvas.appendChild(renderer.domElement)
 
-// Create cube
-const texture = new THREE.TextureLoader().load( 'crate.gif' );
-const geometry = new THREE.BoxGeometry(2, 2, 2)
-const material = new THREE.MeshBasicMaterial( {map: texture } )
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+// Load chair model
+const loader = new GLTFLoader()
+let chair 
 
-camera.position.z = 5
+loader.load("./resources/models/chair.glb", result => {
+    chair = result.scene
+    scene.add(chair)
+})
 
-// Animate cube
-function animate() {
-    requestAnimationFrame(animate)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
 
-    renderer.render(scene, camera)
-}
+// Set camera position and add orbit controls
+let controls = new OrbitControls(camera, renderer.domElement)
+camera.position.set(-10, 8, 10)
+
+// Orbit controls settings
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 2.25
+controls.enableDamping = true
+controls.enablePan = false
+controls.rotateSpeed = 0.75
+controls.maxPolarAngle = 1.2
+controls.minPolarAngle = 1.2
+controls.target = new Vector3(0, 6, 0)
+controls.maxDistance = 11
+controls.update()
+
+// Debug axis
+// scene.add(new AxesHelper(500))
+
+
+// Update renderer
+const animate = function () {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    controls.update()
+};
 
 animate()
