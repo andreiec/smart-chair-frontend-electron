0a24d88f7dc317741cd6201b498a239a894a16d1
Andrei Constantinescu <andreiec112@gmail.com>
2022-01-27 23:51:07 +0200

    Fereasca sfantul duh si maicuta domnului ce am putut sa fac aici

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
-import { HOST } from "../variables.js";
-var request = require('electron-request')
+var request = require('electron-request');
 
+import { HOST } from "../variables.js";
 const { ipcRenderer } = electron
 
 // Title bar minimize and close logic
@@ -17,8 +17,8 @@ minimizeApp.addEventListener("click", () => {
 })
 
 
-// TEST PROMISE TO SET WEIGHT
-const getDataPromise = new Promise(function(resolve, reject) {
+// Promise for user weight
+const getWeightPromise = new Promise(function(resolve, reject) {
     void (async () => {
         const url = HOST + '/weight/measure';
         const defaultOptions = { method: 'GET' };
@@ -32,41 +32,96 @@ const getDataPromise = new Promise(function(resolve, reject) {
     })();
 })
 
-getDataPromise.then(function whenOk(response) {
+// Promise for user info
+const getUserInfoPromise = new Promise(function(resolve, reject) {
+    void (async () => {
+        const url = HOST + '/userInfo';
+        const defaultOptions = { method: 'GET' };
+
+        const response = await request(url, defaultOptions);
+        const response_json = await response.json();
+
+        if (response_json) { resolve(response_json) } else {
+            reject(response)
+        }
+    })();
+})
+
+
+// Buttons and increments for user height
+const user_height_plus = document.getElementById("user-height-plus")
+const user_height_minus = document.getElementById("user-height-minus")
+const user_height_value = document.getElementById("user-height-value")
+
+const chair_height_value = document.getElementById("biometrics-value-chairheight")
+const desk_height_value = document.getElementById("biometrics-value-tableheight")
+
+// TODO more heatings for chair
+const chair_heating_plus = document.getElementById("chair-heating-plus")
+const chair_heating_minus = document.getElementById("chair-heating-minus")
+const chair_heating_value = document.getElementById("chair-heating-value")
+
+
+// Declare user info values and biometrics
+let user_height_value_content 
+let chair_height_value_content
+let desk_height_value_content
+
+
+// Execute promises (weight promise)
+getWeightPromise.then(function whenOk(response) {
     const biometrics_weight = document.getElementById("biometrics-value-weight")
     biometrics_weight.textContent = response['weight']
 }).catch(function notOk(response) {
     console.log("Error in promise " + response)
 })
 
-// END TEST PROMISE WORKS!!!!!!!!
+// Execute promises (userInfo promise)
+getUserInfoPromise.then(function whenOk(response) {
 
-// Buttons and increments
+    if (response['message']) {
+        // No data in db, POST default values
+        void (async () => {
+            const url = HOST + '/userInfo';
+            const defaultOptions = { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({user_height: 175})};
 
-const user_height_plus = document.getElementById("user-height-plus")
-const user_height_minus = document.getElementById("user-height-minus")
-const user_height_value = document.getElementById("user-height-value")
-let user_height_value_content = parseInt(document.getElementById("user-height-value").textContent)
+            const response_2 = await request(url, defaultOptions);
+            let response_2_json = await response_2.json()
+            
+            // This should run only once ever (first time opening the app)
+            if (response_2_json['message'] == "Setari actualizate cu succes") { 
+                // POST Request success, set default values
+                user_height_value_content = 175
+                chair_height_value_content = 46.2
+                desk_height_value_content = 114.2
+                
+                // Set values from db (or default if not found in db)
+                user_height_value.textContent = user_height_value_content
+                chair_height_value.textContent = chair_height_value_content
+                desk_height_value.textContent = desk_height_value_content
+            } else {
+                console.log('FAILED TO POST')
+                console.log(response_2_json)
+            }
+        })();
+    } else {
+        // Data exists already in db, update values
+        user_height_value_content = response['user_height']
+        chair_height_value_content = response['chair_height']
+        desk_height_value_content = response['desk_height']
 
-const table_height_plus = document.getElementById("table-height-plus")
-const table_height_minus = document.getElementById("table-height-minus")
-const table_height_value = document.getElementById("table-height-value")
-let table_height_value_content = parseInt(document.getElementById("table-height-value").textContent)
+        // Set values from db (or default if not found in db)
+        user_height_value.textContent = user_height_value_content
+        chair_height_value.textContent = chair_height_value_content
+        desk_height_value.textContent = desk_height_value_content
+    }
+}).catch(function notOk(response) {
+    console.log("Error in promise " + response)
+})
 
-const chair_heating_plus = document.getElementById("chair-heating-plus")
-const chair_heating_minus = document.getElementById("chair-heating-minus")
-const chair_heating_value = document.getElementById("chair-heating-value")
-let chair_heating_value_content = parseInt(document.getElementById("chair-heating-value").textContent.substring(0, 2))
 
-
-// TODO Make requests
-user_height_value_content = 163
-user_height_value.textContent = user_height_value_content
-
-table_height_value_content = 81
-table_height_value.textContent = table_height_value_content
-
-chair_heating_value_content = 22
+// Set chair heating
+let chair_heating_value_content = 21
 chair_heating_value.textContent = chair_heating_value_content + "°C"
 
 
@@ -82,21 +137,6 @@ user_height_minus.addEventListener("click", () => {
     if (user_height_value_content > 140){
         user_height_value_content--
         user_height_value.textContent = user_height_value_content
-    }
-})
-
-// Table height buttons
-table_height_plus.addEventListener("click", () => {
-    if (table_height_value_content < 120){
-        table_height_value_content++
-        table_height_value.textContent = table_height_value_content
-    }
-})
-
-table_height_minus.addEventListener("click", () => {
-    if (table_height_value_content > 70){
-        table_height_value_content--
-        table_height_value.textContent = table_height_value_content
     }
 })
 
